382ab90fd9deda067e277498df257a4c75e562b9
lambao <baolam12122004@gmail.com>
2025-03-03 09:22:36 +0700

    thWeb1

---
+++ Websidebanhang/Repositories/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Websidebanhang.Models;
+
+namespace Websidebanhang.Repositories
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> GetAllCategories();   
+    }
+}
---
+++ Websidebanhang/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Websidebanhang.Repositories
+{
+    using System.Collections.Generic;
+    using Websidebanhang.Models;
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAll();
+        Product GetById(int id);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(int id);
+    }
+}
