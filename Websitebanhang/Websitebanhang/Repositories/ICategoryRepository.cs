6dbe35228a9c2953f616cacf2e3a27b6d2e3f64b
LamBao <baolam12122004@gmail.com>
2025-03-16 20:09:36 +0700

    16/3

---
+++ Websitebanhang/Websitebanhang/Repositories/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Websitebanhang.Models;
+
+namespace Websitebanhang.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category> GetByIdAsync(int id);
+        Task AddAsync(Category category);
+        Task UpdateAsync(Category category);
+        Task DeleteAsync(int id);
+    }
+    
+}
